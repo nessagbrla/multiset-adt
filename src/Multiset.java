40e88e4d80933d16caaa3b76a3db9c1580511dec
nauraizzahtaufiq <naura.taufiq@mail.utoronto.ca>
2023-09-18 10:37:17 -0400

    modified Multiset

---
+++ src/Multiset.java
@@ -0,0 +1,26 @@
+public class Multiset {
+    public boolean add(int item) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    public void remove(int item) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    public boolean contains(int item) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    public boolean is_empty() {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    public int count(int item) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+}
+
